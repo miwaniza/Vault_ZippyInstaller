bf7dc6b9733131651cbdb714a15677db15734e22
miwaniza <miwaniza@gmail.com>
2014-07-29 16:59:12 +0300

    Commit 1

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WindowsFormsApplication1
+    {
+    partial class Form1
+        {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose ( bool disposing )
+            {
+            if (disposing && (components != null))
+                {
+                components.Dispose();
+                }
+            base.Dispose( disposing );
+            }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent ()
+            {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonUAC = new System.Windows.Forms.Button();
+            this.buttonASP = new System.Windows.Forms.Button();
+            this.buttonIIS = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonReboot = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonTimeout = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonUAC
+            // 
+            this.buttonUAC.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonUAC.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.buttonUAC.Location = new System.Drawing.Point(3, 3);
+            this.buttonUAC.Name = "buttonUAC";
+            this.buttonUAC.Size = new System.Drawing.Size(287, 25);
+            this.buttonUAC.TabIndex = 10;
+            this.buttonUAC.Text = "UAC state";
+            this.buttonUAC.UseVisualStyleBackColor = true;
+            this.buttonUAC.Click += new System.EventHandler(this.buttonUAC_Click);
+            // 
+            // buttonASP
+            // 
+            this.buttonASP.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonASP.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.buttonASP.Location = new System.Drawing.Point(3, 65);
+            this.buttonASP.Name = "buttonASP";
+            this.buttonASP.Size = new System.Drawing.Size(287, 27);
+            this.buttonASP.TabIndex = 12;
+            this.buttonASP.Text = "Re-register ASP .Net filters";
+            this.buttonASP.UseVisualStyleBackColor = true;
+            this.buttonASP.Click += new System.EventHandler(this.buttonASP_Click);
+            // 
+            // buttonIIS
+            // 
+            this.buttonIIS.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonIIS.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.buttonIIS.Location = new System.Drawing.Point(3, 34);
+            this.buttonIIS.Name = "buttonIIS";
+            this.buttonIIS.Size = new System.Drawing.Size(287, 25);
+            this.buttonIIS.TabIndex = 11;
+            this.buttonIIS.Text = "Install Vault-related IIS features";
+            this.buttonIIS.UseVisualStyleBackColor = true;
+            this.buttonIIS.Click += new System.EventHandler(this.buttonIIS_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.buttonReboot, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.buttonUAC, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonASP, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonIIS, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 6);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(293, 302);
+            this.tableLayoutPanel1.TabIndex = 13;
+            // 
+            // buttonReboot
+            // 
+            this.buttonReboot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonReboot.Location = new System.Drawing.Point(3, 98);
+            this.buttonReboot.Name = "buttonReboot";
+            this.buttonReboot.Size = new System.Drawing.Size(287, 27);
+            this.buttonReboot.TabIndex = 14;
+            this.buttonReboot.Text = "Hack \"Reboot Needed\"";
+            this.buttonReboot.UseVisualStyleBackColor = true;
+            this.buttonReboot.Click += new System.EventHandler(this.buttonReboot_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonTimeout);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 151);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 148);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "IIS sites with timeout < 900s";
+            // 
+            // buttonTimeout
+            // 
+            this.buttonTimeout.Location = new System.Drawing.Point(6, 106);
+            this.buttonTimeout.Name = "buttonTimeout";
+            this.buttonTimeout.Size = new System.Drawing.Size(272, 42);
+            this.buttonTimeout.TabIndex = 17;
+            this.buttonTimeout.Text = "Increase IIS timeout for selected site";
+            this.buttonTimeout.UseVisualStyleBackColor = true;
+            this.buttonTimeout.Click += new System.EventHandler(this.buttonTimeout_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 18);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(272, 82);
+            this.listBox1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 302);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Vault Zippy Installer";
+            this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.Form1_HelpButtonClicked);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+            }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonUAC;
+        private System.Windows.Forms.Button buttonASP;
+        private System.Windows.Forms.Button buttonIIS;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonReboot;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonTimeout;
+
+
+
+        }
+    }
+
---
+++ Form1.cs
@@ -0,0 +1,501 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Globalization;
+using System.Management;
+using Microsoft.Web.Administration;
+
+
+
+namespace WindowsFormsApplication1
+    {
+    public partial class Form1 : Form
+        {
+        
+        public Form1 ()
+            {
+            InitializeComponent();
+            InitForm();
+            _syncContext = SynchronizationContext.Current;
+            CheckUacState();
+            CheckRebootNeeded();
+            CheckIISState();
+            CheckIISTimeoutState();
+            
+            }
+
+ 
+
+        private void InitForm ()
+            {
+            System.Resources.ResourceManager resources = new
+            System.Resources.ResourceManager( typeof( Form1 ) );
+                       }
+
+        private void CheckUacState ()
+            {
+            if (!GetRegValueBoolState(@"Software\Microsoft\Windows\CurrentVersion\policies\system", "EnableLUA"))
+            {
+                buttonUAC.Text = "UAC is OFF - OK";
+                buttonUAC.BackColor = Color.LightGreen;
+                buttonUAC.Refresh();
+            }
+            else
+            {
+                buttonUAC.Text = "UAC is ON - Click to disable";
+                buttonUAC.BackColor = Color.OrangeRed;
+                buttonUAC.Refresh();
+            };
+            }
+
+        private void GetIISTimeoutState()
+        {
+            listBox1.Items.Clear();
+            double timeout;
+            using (ServerManager serverManager = new ServerManager())
+            
+            {
+                Configuration config = serverManager.GetApplicationHostConfiguration();
+                ConfigurationSection sitesSection = config.GetSection("system.applicationHost/sites");
+                ConfigurationElementCollection allSites = sitesSection.GetCollection();
+                
+                foreach(ConfigurationElement site in allSites)
+                {
+                    
+                    ConfigurationElement limitsElement = site.GetChildElement("limits");
+                    TimeSpan t = (TimeSpan) limitsElement["connectionTimeout"];
+                    timeout = t.TotalSeconds;
+                    if (timeout < 900)
+                    {
+                        listBox1.Items.Add(site["name"] );
+
+                    }
+                    
+                }
+               
+            }
+            
+            
+        }
+
+        private void CheckIISTimeoutState()
+        {
+            GetIISTimeoutState();
+
+            if (listBox1.Items.Count == 0)
+            {
+                buttonTimeout.Text = "All sites' timeouts are greater then 900s - OK";
+                buttonTimeout.BackColor = Color.LightGreen;
+                buttonTimeout.Refresh();
+            }
+            else
+            {
+                buttonTimeout.Text = "Select site and from list\nand click here to set timeout to 900s";
+                buttonTimeout.BackColor = Color.OrangeRed;
+                buttonTimeout.Refresh();
+            };
+           
+        }
+
+
+
+        private bool GetRegRebootNeeded()
+            {
+                
+                RegistryKey key = Registry.LocalMachine.OpenSubKey(@"System\CurrentControlSet\Control\Session Manager", false);
+
+                // get value
+                object value = key.GetValue("PendingFileRenameOperations", null, RegistryValueOptions.None);
+            
+                if (value == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                };
+        }
+
+        private void CheckRebootNeeded()
+        {
+            if (!GetRegRebootNeeded())
+            {
+                buttonReboot.Text = "Reboot not needed - OK";
+                buttonReboot.BackColor = Color.LightGreen;
+                buttonReboot.Refresh();
+            }
+            else
+            {
+                buttonReboot.Text = "Reboot needed - Click to disable";
+                buttonReboot.BackColor = Color.OrangeRed;
+                buttonReboot.Refresh();
+            };
+        }
+
+        private void CheckIISState()
+        {
+            if (GetRegIISState().Count==0)
+            {
+                buttonIIS.Text = "IIS installation not needed - OK";
+                buttonIIS.BackColor = Color.LightGreen;
+                buttonIIS.Refresh();
+            }
+            else
+            {
+                buttonIIS.Text = "IIS installation needed - Click to start";
+                buttonIIS.BackColor = Color.OrangeRed;
+                buttonIIS.Refresh();
+            };
+        }
+
+        private bool GetRegValueBoolState(string subKey, string keyValue)
+        {
+
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(subKey, false);
+            object value = key.GetValue(keyValue, 0, RegistryValueOptions.None);
+            return Convert.ToBoolean(value);
+
+        }
+
+        private void buttonASP_Click ( object sender, EventArgs e )
+            {
+            ReRegisterASPFilters();
+            }
+
+        private void ReRegisterASPFilters ()
+            {
+            
+            try
+                {
+                string windir = Environment.GetEnvironmentVariable("windir");
+                Process first = Process.Start( windir+@"\microsoft.net\framework\v4.0.30319\aspnet_regiis.exe", "-i -enable" );
+                first.WaitForExit();
+                Process second = Process.Start( windir + @"\microsoft.net\framework64\v4.0.30319\aspnet_regiis.exe", "-i -enable");
+                second.WaitForExit();
+                }
+            catch
+                {
+                MessageBox.Show( "Smth. wrong" );
+                }
+            finally
+                {
+                MessageBox.Show( "Done!" );
+                }
+            }
+
+
+        public SynchronizationContext _syncContext { get; set; }
+
+        private void buttonIIS_Click ( object sender, EventArgs e )
+            {
+            InstallIIS();
+            }
+
+        private void InstallIIS ()
+            {
+
+            List<string> FeaturesToInstall = GetRegIISState();
+
+            string str = "";
+            if (FeaturesToInstall.Count == 0)
+            {
+                MessageBox.Show("All IIS features are already installed");
+                return;
+            };
+            foreach (string feature in FeaturesToInstall)
+            {
+                str += " - " + feature + "\n";
+            };
+
+            FeaturesToInstall.Add("IIS-ManagementConsole"); 
+            FeaturesToInstall.Add("IIS-WebServerManagementTools");
+            FeaturesToInstall.Add("IIS-Performance");
+            FeaturesToInstall.Add("IIS-HealthAndDiagnostics");
+            FeaturesToInstall.Add("IIS-IIS6ManagementCompatibility");
+            FeaturesToInstall.Add("WAS-WindowsActivationService");
+            
+            DialogResult dr = MessageBox.Show("This operation will install next features:\n" + str, "IIS installation", MessageBoxButtons.OKCancel);
+            if (dr == DialogResult.OK)
+            {
+                string cmdargument = "";
+                foreach (string feature in FeaturesToInstall)
+                {
+                    cmdargument += @" /FeatureName:" + feature;
+                };
+
+                try
+                {
+                    Process proc = new Process();
+                    proc.StartInfo.UseShellExecute = false;
+                    proc.StartInfo.FileName=Environment.GetEnvironmentVariable("windir") + "\\SysNative\\dism.exe";
+                    proc.StartInfo.Arguments=@"/Online /Enable-Feature" + cmdargument;
+                    
+                    proc.Start();
+                    
+                    proc.WaitForExit();
+                    
+                    proc.Close();
+                }
+                catch
+                {
+                    MessageBox.Show("Smth. wrong");
+                }
+                finally
+                {
+                    MessageBox.Show("Done!");
+                };
+            };
+            CheckIISState();
+            }
+
+        private List<string> GetRegIISState()
+        {
+            string[,] input = { { "IIS-WebServerRole", "W3SVC" },
+                                    {"IIS-ASP","ASPNET"},
+                                    {"IIS-Metabase","Metabase"},
+                                    //{"IIS-IIS6ManagementCompatibility"," "},
+                                    //{"IIS-ManagementConsole","LegacySnapin"},
+                                    {"IIS-NetFxExtensibility","NetFxExtensibility"},
+                                    {"IIS-ISAPIExtensions","ISAPIExtensions"},
+                                    {"IIS-ISAPIFilter","ISAPIFilter"},
+                                    {"IIS-DefaultDocument","DefaultDocument"},
+                                    {"IIS-DirectoryBrowsing","DirectoryBrowse"},
+                                    {"IIS-HttpErrors","HttpErrors"},
+                                    {"IIS-StaticContent","HttpCompressionStatic"},
+                                    {"IIS-HttpLogging","HttpLogging"},
+                                    {"IIS-RequestMonitor","RequestMonitor"},
+                                    {"IIS-HttpCompressionStatic","HttpCompressionStatic"},
+                                    {"IIS-RequestFiltering","RequestFiltering"},
+                                    //{"WAS-WindowsActivationService"," "},
+                                    {"WAS-ProcessModel","ProcessModel"},
+                                    {"WAS-NetFxEnvironment","NetFxEnvironment"},
+                                    {"WAS-ConfigurationAPI", "WASConfigurationAPI" }};
+
+
+            List<string> FeaturesToInstall = new List<string>();
+            
+            FeaturesToInstall.AddRange(FilterInstalledFeatures(input));
+            return FeaturesToInstall;
+        }
+
+
+        
+
+        private IEnumerable<string> FilterInstalledFeatures(string[,] input)
+        {
+            List<string> FeaturesToInstall = new List<string>();
+
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                if (!GetRegValueBoolState(@"Software\Microsoft\InetStp\Components\", input[i, 1]))
+                {
+                    FeaturesToInstall.Add(input[i, 0]);
+                }
+            }
+            
+
+            
+            return FeaturesToInstall;
+        }
+
+    
+
+        private void buttonUAC_Click ( object sender, EventArgs e )
+            {
+            bool UACstate = GetRegValueBoolState(@"Software\Microsoft\Windows\CurrentVersion\policies\system", "EnableLUA");
+            SetRegUacState( !UACstate );
+            CheckUacState();
+            }
+
+        private void Form1_Load ( object sender, EventArgs e )
+            {
+            
+            }
+
+        private void Form1_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            DisplayHelp();
+           
+        }
+
+        private void DisplayHelp()
+        {
+            string filename = "Vault Zippy Installer Help.txt";
+            if (!System.IO.File.Exists(filename))
+                System.IO.File.WriteAllText(filename, Properties.Resources.Help);
+            System.Diagnostics.Process.Start(filename);
+        }
+
+        private static void Export(string exportPath, string registryPath)
+        {
+            string path = "\"" + exportPath + "\"";
+            string key = "\"" + registryPath + "\"";
+            Process proc = new Process();
+
+            try
+            {
+                proc.StartInfo.FileName = "regedit.exe";
+                proc.StartInfo.UseShellExecute = false;
+
+                proc = Process.Start("regedit.exe", "/e " + path + " " + key + "");
+                proc.WaitForExit();
+            }
+            catch (Exception)
+            {
+                proc.Dispose();
+            }
+        }
+
+        private void buttonReboot_Click(object sender, EventArgs e)
+        {
+            if (GetRegRebootNeeded())
+            {
+                HackRebootNeeded();
+            }
+            else
+            {
+                MessageBox.Show("Everything is OK - just relax!");
+            }
+            CheckRebootNeeded();
+        }
+
+        private void SetRegUacState(bool state)
+        {
+            if (state == false)
+            {
+                DialogResult dr = MessageBox.Show("This will disable UAC", "Attention", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+                if (dr == DialogResult.OK)
+                {
+                    Registry.SetValue(@"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\policies\system", "EnableLUA", Convert.ToInt32(state));
+                }
+            }
+            else
+            {
+                Registry.SetValue(@"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\policies\system", "EnableLUA", Convert.ToInt32(state));
+            }
+        }
+
+        private void SetIISTimeout(string IISsiteName, double timeout)
+        {
+            using (ServerManager serverManager = new ServerManager())
+            {
+                Configuration config = serverManager.GetApplicationHostConfiguration();
+                ConfigurationSection sitesSection = config.GetSection("system.applicationHost/sites");
+                ConfigurationElementCollection sitesCollection = sitesSection.GetCollection();
+
+                ConfigurationElement siteElement = FindElement(sitesCollection, "site", "name", @"Default Web Site");
+                if (siteElement == null) throw new InvalidOperationException("Element not found!");
+
+                ConfigurationElement limitsElement = siteElement.GetChildElement("limits");
+                limitsElement["connectionTimeout"] = TimeSpan.FromSeconds(timeout);
+
+                serverManager.CommitChanges();
+            }
+        }
+
+        private static ConfigurationElement FindElement(ConfigurationElementCollection collection, string elementTagName, params string[] keyValues)
+        {
+            foreach (ConfigurationElement element in collection)
+            {
+                if (String.Equals(element.ElementTagName, elementTagName, StringComparison.OrdinalIgnoreCase))
+                {
+                    bool matches = true;
+                    for (int i = 0; i < keyValues.Length; i += 2)
+                    {
+                        object o = element.GetAttributeValue(keyValues[i]);
+                        string value = null;
+                        if (o != null)
+                        {
+                            value = o.ToString();
+                        }
+                        if (!String.Equals(value, keyValues[i + 1], StringComparison.OrdinalIgnoreCase))
+                        {
+                            matches = false;
+                            break;
+                        }
+                    }
+                    if (matches)
+                    {
+                        return element;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void HackRebootNeeded()
+        {
+            DialogResult dr = MessageBox.Show("This operation will delete registry key value. Create a backup?\n - Yes - to delete with backup.\n - No - just delete.\n - Cancel - to abort.", "Attention", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Exclamation);
+
+            switch (dr)
+            {
+                case DialogResult.Yes:
+                    {
+                        SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+                        saveFileDialog1.Filter = "All files (*.*)|*.*|Registry files (*.reg)|*.reg";
+                        saveFileDialog1.FilterIndex = 2;
+                        saveFileDialog1.RestoreDirectory = true;
+                        saveFileDialog1.OverwritePrompt = true;
+                        saveFileDialog1.FileName = "PendingFileRenameOperations_bak";
+                        
+                        if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                        {
+                            Export(saveFileDialog1.FileName, @"HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager");  
+                        }
+
+                        RegistryKey key = Registry.LocalMachine.OpenSubKey(@"System\CurrentControlSet\Control\Session Manager", true);
+                        key.DeleteValue("PendingFileRenameOperations", false);
+                        
+                    }
+                    break;
+                case DialogResult.No:
+                    {
+                        RegistryKey key = Registry.LocalMachine.OpenSubKey(@"System\CurrentControlSet\Control\Session Manager", true);
+                        key.DeleteValue("PendingFileRenameOperations", false);
+                    }
+                    break;
+                case DialogResult.Cancel:
+                    break;
+                default:
+                    break;
+            };
+
+            
+
+        }
+
+        private void buttonTimeout_Click(object sender, EventArgs e)
+        {
+            if ((listBox1.SelectedItem != null)&&(listBox1.Items.Count>0))
+            {
+                SetIISTimeout(listBox1.SelectedItem.ToString(), 900);
+                MessageBox.Show("Timeout increased to 900s");
+            }
+            if ((listBox1.SelectedItem == null) && (listBox1.Items.Count > 0))
+            {
+                MessageBox.Show("Select site, please");
+            }
+
+            if ((listBox1.SelectedItem == null) && (listBox1.Items.Count == 0))
+            {
+                MessageBox.Show("Keep calm - all's O.K.");
+            }
+
+            CheckIISTimeoutState();
+        }
+
+
+
+        }
+    }
